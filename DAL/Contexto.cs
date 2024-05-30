d862db131276bbc0fcd98be665f03a596c8c72a6
Cape23180909 <samilgamer@outlook.com>
2024-05-30 07:23:42 -0400

    Preparando el examen

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -4,5 +4,9 @@ namespace Aplicada.DAL
 {
     public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+
+        //public DbSet <>
     }
 }
