8432df578ce5718616c42db861bea4ecdc85c068
Cape23180909 <samilgamer@outlook.com>
2024-06-03 18:24:54 -0400

    Colocando las propiedades del proyecto

---
+++ Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aplicada.Models
+{
+    public class Articulos
+    {
+        
+        [Key]
+        //ArticuloId, Descripcion, Costo, Ganancia y precio
+        public int ArticuloId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal? Costo { get; set; }
+        public decimal? Ganancia { get; set; }
+        public decimal? Precio { get; set; }
+
+        
+
+
+    }
+}
