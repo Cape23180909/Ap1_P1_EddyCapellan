39781789c23b91bf7fa9ba3ee3c89f06fd69a3a2
Cape23180909 <samilgamer@outlook.com>
2024-06-03 18:28:23 -0400

    Modelo completo

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -8,12 +8,14 @@ namespace Aplicada.Models
         [Key]
         //ArticuloId, Descripcion, Costo, Ganancia y precio
         public int ArticuloId { get; set; }
+        [Required (ErrorMessage = "Mio, coloque una descripcion")]
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Mio, coloque un costo")]
         public decimal? Costo { get; set; }
+        [Required(ErrorMessage = "Mio, coloque una ganancia")]
         public decimal? Ganancia { get; set; }
+        [Required(ErrorMessage = "Mio, coloque un precio")]
         public decimal? Precio { get; set; }
-
-        
 
 
     }
