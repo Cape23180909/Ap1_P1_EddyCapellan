8b823cd08c7c14fee89f114f0a7d0e4c795e305e
Cape23180909 <samilgamer@outlook.com>
2024-06-03 19:55:33 -0400

    Colocando la ruta donde lo va a llevar el dicho componente

---
+++ Services/ArticuloService.cs
@@ -0,0 +1,70 @@
+﻿using Aplicada.DAL;
+using Microsoft.EntityFrameworkCore;
+using Aplicada.Models;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Diagnostics.Metrics;
+
+namespace Aplicada.Services
+{
+    public class ArticuloService
+    {
+        private readonly Contexto Contexto;
+        public ArticuloService (Contexto contexto)
+        {
+            Contexto = contexto;
+        }
+        //Metodo Existe 
+        public async Task <bool> Existe (int ArticuloId)
+        {
+            return await Contexto.articulos.AnyAsync (a =>a.ArticuloId == ArticuloId);
+        }
+        //Metodo Insertar
+        private async Task <bool> Insertar (Articulos articulos)
+        {
+            Contexto.articulos.Add (articulos);
+            return await Contexto.SaveChangesAsync () > 0;
+        }
+        //Metodo modificar
+        private async Task <bool> Modificar (Articulos articulos)
+        {
+            Contexto.articulos.Update(articulos);
+            var Modificado = await Contexto.SaveChangesAsync () > 0;
+            Contexto.Entry(articulos).State = EntityState.Detached;
+            return Modificado;
+             
+        }
+        //Metodo Guardar
+        public async Task <bool> Guardar (Articulos articulos)
+        {
+            if (!await Existe (articulos.ArticuloId))
+                return await Insertar (articulos);
+            else 
+                return await Modificar (articulos);
+        }
+        //Metodo eliminar 
+        public async Task <bool> Eliminar (int id)
+        {
+            var EliminarArticulo = await Contexto.articulos
+                .Where(a =>a.ArticuloId==id)
+                .ExecuteDeleteAsync();
+            return EliminarArticulo > 0;
+        }
+        // Metodo buscar 
+        public async Task <Articulos?> Buscar (int id)
+        {
+            return await Contexto.articulos
+                .AsNoTracking()
+                .FirstOrDefaultAsync ();
+        }
+        //Metodo Listar 
+        public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+        {
+            return await Contexto.articulos
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
